1938cd834afccd9af792e3d6d45b16189b325df4
shiva650897 <sk650897@gmail.com>
2023-06-24 11:09:20 +0530

    Added exception and logger

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+import  sys
+
+def error_message_details(error,error_detail:sys):
+
+	_,_,exc_tb = error_detail.exc_info() # not interested first 2 values
+	filename = exc_tb.tb_frame.f_code.co_filename
+	error_message = "Error occured in python script name [{0}] line number [{1}] error_message [{2}]".foramt(
+	file_name,exc_tb,tb_lineno,str(error)
+	)
+	return error_message
+
+class CustomException(Exception):
+	def__init__(self,error_message,error_detail:sys):
+		super.__init__(error_message)
+		self.error_message=error_message_details(error_message,error_detail=error_detail)
+
+
+	def __str__(self):
+		return self.error_message
+
+if __name__="main":
+	CustomException(Exception,sys)
+
